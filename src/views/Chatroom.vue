e95b99155054e8e85c7b23ba137575aef7e0b0e5
Ionut Alexandru Muresan <alexandru.muresan@fortech.ro>
2022-01-12 14:31:27 +0200

    115. CHALLENGE - Watching the Current User

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -1,16 +1,30 @@
 <template>
   <div class="container">
-      <Navbar />
+    <Navbar />
   </div>
 </template>
 
 <script>
+import { watch } from "@vue/runtime-core";
 import Navbar from "../components/Navbar.vue";
+import getUser from "../composables/getUser";
+import { useRouter } from "vue-router";
 export default {
   components: { Navbar },
+  setup() {
+    const { user } = getUser();
+    const router = useRouter();
+
+    watch(user, () => {
+      if (!user.value) {
+        router.push({ name: "Welcome" });
+      }
+    });
+
+    return { user };
+  },
 };
 </script>
 
 <style>
-
 </style>
